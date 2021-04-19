c4222d43587eac287541922997e6b0de0a6b6ed8
armaan4658 <80997749+armaan4658@users.noreply.github.com>
2021-04-19 09:46:18 +0530

    Update duplicate.js

--- duplicate.js
+++ duplicate.js
@@ -1,6 +1,27 @@
 var names = ["Mike","Matt","Nancy","Adam","Jenny","Nancy","Carl"];
-var uniqueNames = [];
-$.each(names, function(i, el){
-    if($.inArray(el, uniqueNames) === -1) uniqueNames.push(el);
-});
-console.log(names);
+//creating anonymous function
+let unique = function(name){
+    let uniqueChars = [];
+    names.forEach((c) => {
+        if (!uniqueChars.includes(c)) {
+            uniqueChars.push(c);
+        }
+    });
+    //printing out the output
+    console.log("-------------using anonymous function------------");
+    console.log(`unique name = ${uniqueChars.join(" ,")}`);
+}
+//calling the anonymous function
+unique(names);
+//creating IIFE function
+(function(name){
+    let uniqueChars = [];
+    names.forEach((c) => {
+        if (!uniqueChars.includes(c)) {
+            uniqueChars.push(c);
+        }
+    });
+    //printing out the output
+    console.log("---------------using IIFE function ----------");
+    console.log(`unique name = ${uniqueChars.join(" ,")}`);
+})(names);
